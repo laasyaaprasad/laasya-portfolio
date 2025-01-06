7f5f62abcc6d3d3f374a19b54caa8ed3af47930b
laasyaaprasad <75083241+laasyaaprasad@users.noreply.github.com>
2025-01-05 20:11:19 -0800

    Updates

---
+++ precache-manifest.01acf910fc44463ef98be912fa8a0e32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22f1bd00b852de581e8e1b50f1c33132",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91907d340a73753ce4ff",
+    "url": "/static/css/2.c014aa9f.chunk.css"
+  },
+  {
+    "revision": "b2dd84426da441752300",
+    "url": "/static/css/main.8a959592.chunk.css"
+  },
+  {
+    "revision": "91907d340a73753ce4ff",
+    "url": "/static/js/2.bc859dd8.chunk.js"
+  },
+  {
+    "revision": "46a379a572333860f78765c743603e43",
+    "url": "/static/js/2.bc859dd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2dd84426da441752300",
+    "url": "/static/js/main.d1905f6b.chunk.js"
+  },
+  {
+    "revision": "430f72daf7c3a4d42642",
+    "url": "/static/js/runtime-main.351e9c47.js"
+  }
+]);
